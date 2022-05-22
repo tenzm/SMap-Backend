d304c9ced224d4d0fb6fe30b32764452a65243f1
Maksim Blinov <tenzm@mail.ru>
2022-05-22 17:41:05 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Union
+import uvicorn
+from fastapi import FastAPI
+import json
+
+#todo: Переделать на бд
+with open('./SberMap/data/positions.txt') as json_file:
+    data = json.load(json_file)
+data.keys()
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="localhost", reload=True)
